1960fcc601349eb600c057ac6f3caf7c2f236402
iam-sachinverma <sachinvermab@gmail.com>
2022-05-14 02:34:12 +0530

    frontend

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 
-
 const PORT = process.env.PORT || 3000;  
 
 // json data parser
@@ -19,12 +18,15 @@ connectDB();
 const corsOptions = {
     origin: process.env.ALLOWED_CLIENTS.split(',')
 }
-app.use(cors(corsOptions));
-
+app.use(cors(corsOptions))
 
 // Templates engine
 app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'ejs');
+
+app.get('', (req, res) => {
+    res.render('index')
+});
 
 // Routes
 app.use('/api/files', require('./routes/files'));
